c73e95390a0c90a00528880425ae46d541e4e665
eduardopacompialopez <eduardopacompialopez@gmail.com>
2022-02-25 23:22:22 -0500

    COMPLETO HASTA EL VIDEO 84

--- tiendapersonal/tiendapersonal/Models/TipoCuenta.cs
+++ tiendapersonal/tiendapersonal/Models/TipoCuenta.cs
@@ -8,8 +8,27 @@ namespace tiendapersonal.Models
 
         [Required(ErrorMessage ="El campo {0} es requerido")]
         [StringLength(maximumLength:50,MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1} caracteres")]
+        [Display(Name ="Nombre del tipo cuenta")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        /*Prueba de otras validaciones por defecto */
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage ="El campo debe ser un correo electronico valido")]
+        public string Email { get; set; }
+
+        [Range(minimum:18, maximum:120,ErrorMessage ="El valor debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+
+        [Url(ErrorMessage ="El campo debe ser una URL valida")]
+        public string Url { get; set; }
+
+        [CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        [Display(Name ="Tarjeta de Credito")]
+        public string TarjetaDeCredito { get; set; }
+
+
+
     }
 }
