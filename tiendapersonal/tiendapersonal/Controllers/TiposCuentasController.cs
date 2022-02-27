cb3cc862991661f77345d04d321af3b8b2d39679
eduardopacompialopez <eduardopacompialopez@gmail.com>
2022-02-26 22:26:48 -0500

    COMPLETO HASTA EL VIDEO 88

--- tiendapersonal/tiendapersonal/Controllers/TiposCuentasController.cs
+++ tiendapersonal/tiendapersonal/Controllers/TiposCuentasController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using tiendapersonal.Models;
 
 namespace tiendapersonal.Controllers
 {
     public class TiposCuentasController:Controller
     {
+        private readonly string connectionString;
+        //CONSTRUCTOR
+        public TiposCuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+
         public IActionResult Crear()
         {
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            }
+
             return View();
         }
 
--- tiendapersonal/tiendapersonal/Models/TipoCuenta.cs
+++ tiendapersonal/tiendapersonal/Models/TipoCuenta.cs
@@ -3,17 +3,26 @@ using tiendapersonal.Validaciones;
 
 namespace tiendapersonal.Models
 {
-    public class TipoCuenta
+    public class TipoCuenta : IValidatableObject
     {
         public int Id { get; set; }
 
         [Required(ErrorMessage ="El campo {0} es requerido")]
-        [PrimeraLetraMayuscula]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
 
-       
-
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Nombre!=null &&  Nombre.Length>0)
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if (primeraLetra !=primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula", new[] { nameof(Nombre)});
+                }
+            }
+        }
     }
 }
