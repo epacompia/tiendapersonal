96840a89f813a3c1b8b99d8093598b52ede10cf1
eduardopacompialopez <eduardopacompialopez@hotmail.com>
2022-03-22 16:53:25 -0500

    SUBIENDO HASTA EL VIDEO 104 LA MITAD

---
+++ tiendapersonal/tiendapersonal/Controllers/CuentasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tiendapersonal.Controllers
+{
+    public class CuentasController:Controller
+    {
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
